3c82ede1eb724d112d7c1252b14a0c788569465e
chenhh <chenhh@csizg.com>
2022-02-22 16:36:55 +0800

    MOD: config json .

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,33 @@
 #!/bin/sh
 
 # Global variables
-DIR_CONFIG="/etc/v2ray"
+DIR_CONFIG="/home/rku/AppData/v2fly"
 DIR_RUNTIME="/usr/bin"
 DIR_TMP="$(mktemp -d)"
 
-# Write V2Ray configuration
-cat << EOF > ${DIR_TMP}/heroku.json
+# Write v2fly configuration
+cat << EOF > ${DIR_TMP}/rku.json
 {
+    "log": {
+        "loglevel": "warning"
+    },
     "inbounds": [{
         "port": ${PORT},
         "protocol": "vmess",
         "settings": {
             "clients": [{
                 "id": "${ID}",
-                "alterId": 0
+                "email": "vmess_ws@v2fly.com",
+                "level": 0
             }]
         },
         "streamSettings": {
             "network": "ws",
+            "security": "none",
             "wsSettings": {
-                "path": "${WSPATH}"
+                "acceptProxyProtocol": false,
+                "path": "${WSPATH}",
+                "maxEarlyData": 2048
             }
         }
     }],
@@ -30,17 +37,17 @@ cat << EOF > ${DIR_TMP}/heroku.json
 }
 EOF
 
-# Get V2Ray executable release
-curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
-busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
+# Get v2fly executable release
+curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o ${DIR_TMP}/v2fly_dist.zip
+busybox unzip ${DIR_TMP}/v2fly_dist.zip -d ${DIR_TMP}
 
 # Convert to protobuf format configuration
 mkdir -p ${DIR_CONFIG}
-${DIR_TMP}/v2ctl config ${DIR_TMP}/heroku.json > ${DIR_CONFIG}/config.pb
+${DIR_TMP}/v2ctl config ${DIR_TMP}/rku.json > ${DIR_CONFIG}/config.pb
 
-# Install V2Ray
+# Install v2fly
 install -m 755 ${DIR_TMP}/v2ray ${DIR_RUNTIME}
 rm -rf ${DIR_TMP}
 
-# Run V2Ray
+# Run v2fly
 ${DIR_RUNTIME}/v2ray -config=${DIR_CONFIG}/config.pb
